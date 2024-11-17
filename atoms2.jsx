1bd14c6e8978b0b29d47e8c301e7bcfaee4f3834
Pratham Bhatia <83857889+prathambhatia@users.noreply.github.com>
2024-11-17 14:11:33 +0530

    Create atoms2.jsx

---
+++ atoms2.jsx
@@ -0,0 +1,64 @@
+import './App.css'
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { notifications, totalNotificationSelector } from './atoms'
+import { useEffect } from 'react'
+import axios from 'axios'
+
+function App() {
+  return <RecoilRoot>
+    <MainApp />
+  </RecoilRoot>
+}
+
+function MainApp() {
+  const [networkCount, setNetworkCount] = useRecoilState(notifications)
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
+
+  useEffect(() => {
+    // fetch
+    axios.get("http://localhost:3001/notifications")
+      .then(res => {
+        setNetworkCount(res.data)
+      })
+  }, [])
+
+  return (
+    <>
+      <button>Home</button>
+      
+      <button>My network ({networkCount.network >= 100 ? "99+" : networkCount.network})</button>
+      <button>Jobs {networkCount.jobs}</button>
+      <button>Messaging ({networkCount.messaging})</button>
+      <button>Notifications ({networkCount.notifications})</button>
+
+      <button>Me ({totalNotificationCount})</button>
+    </>
+  )
+}
+
+export default App
+
+
+//atom.js
+import { atom, selector } from "recoil";
+
+export const notifications = atom({
+    key: "networkCount",
+    default: {
+        network: 0,
+        jobs: 0,
+        messaging: 0,
+        notifications: 0
+    }
+});
+
+export const totalNotificationSelector = selector({
+    key: "totalNotificationSelector",
+    get: ({get}) => {
+        const allNotifications = get(notifications);
+        return allNotifications.network +
+        allNotifications.jobs +
+        allNotifications.messaging +
+        allNotifications.notifications
+    }
+});
